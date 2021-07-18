3da9a03aac3589f5edec9a37352b68110bae0270
Peter deHaan <peter@deseloper.com>
2021-07-18 14:57:47 -0700

    Add options for always lowercase/uppercase chars

--- lib.js
+++ lib.js
@@ -1,10 +1,22 @@
-module.exports = function toSpongeBobCase(str = "") {
-  // Lowercase the string and convert into an array of characters.
-  const chars = str.toString().toLowerCase().split("");
-  // Uppercase every second character.
-  for (let idx = 0; idx < chars.length; idx += 2) {
-    chars[idx] = chars[idx].toUpperCase();
+module.exports = function toSpongeBobCase(str = "", alwaysLowerCase = ["i", "o"], alwaysUpperCase= ["l"]) {
+  if (!Array.isArray(alwaysLowerCase)) {
+    alwaysLowerCase = Array.from(alwaysLowerCase.toString());
   }
+  if (!Array.isArray(alwaysUpperCase)) {
+    alwaysUpperCase = Array.from(alwaysUpperCase.toString());
+  }
+  // Lowercase the string and convert into an array of characters.
+  const chars = Array.from(str.toLowerCase())
+    // Loop through each character and uppercase the odd letters.
+    .map((char, idx) => {
+      if (alwaysLowerCase.includes(char)) {
+        return char;
+      }
+      if (alwaysUpperCase.includes(char) || idx % 2) {
+        return char = char.toUpperCase();
+      }
+      return char;
+    });
   // Convert the array of characters back into a string.
   return chars.join("");
 };
